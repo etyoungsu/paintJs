5497d1b2022e07076bf0e812de26cee9c595c5f0
etyoungsu <etyoungsu@ajou.ac.kr>
2021-02-25 23:39:15 +0900

    Brush Color & Size & Fill & PresentColor

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const fill = document.getElementById("jsMode");
+const present = document.getElementsByClassName("jsPresent");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -8,6 +12,7 @@ ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let fillingAll = false;
 
 function stopPainting() {
     painting = false;
@@ -30,12 +35,42 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
-}
-
 function onMouseUp(event) {
     stopPainting();
+}
+
+function handleColorClick(event) {
+    const background = event.target.style.backgroundColor;
+    if (!fillingAll) {
+        ctx.strokeStyle = background;
+        present[0].style.backgroundColor = background;
+    }
+    else {
+        ctx.fillStyle = background;
+        ctx.fillRect(0, 0, 700, 700);
+        endFill();
+    }
+}
+
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function endFill() {
+    fill.textContent = "FILL";
+    fillingAll = false;
+}
+
+function handleFill() {
+    if (!fillingAll) {
+        fill.textContent = "Paint";
+        fillingAll = true;
+    }
+    else {
+        fill.textContent = "FILL";
+        fillingAll = false;
+    }
 }
 
 if (canvas) {
@@ -44,3 +79,9 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+range.addEventListener("input", handleRangeChange);
+
+fill.addEventListener("click", handleFill);
